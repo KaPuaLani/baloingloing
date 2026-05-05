ce82c516819a13fa5b81c46cb05a0d3d86f0568d
Lani <gustsomeperson@gmail.com>
2026-05-05 10:10:49 -0700

    uhhh broken we are so dead

---
+++ Assets/Scenes/LB/Menu/codededededede/LoadNextLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadNextLevel : MonoBehaviour
+{
+    public string levelToLoad;
+    //when the player collides with me
+    //load the given level
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("foo");
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/LB/Menu/codededededede/PauseMenu.cs
+++ Assets/Scenes/LB/Menu/codededededede/PauseMenu.cs
@@ -10,30 +10,53 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField]
     public string levelToLoad;
+    public Canvas UI;
+    public Canvas pUI;
+    public Camera UIcam;
+    public Camera cam;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Canvas>().enabled = false;
+        //GetComponent<Canvas>().enabled = false;
     }
-
+    public void OnPause(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            //pause the game
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0;
+            //show our pause menu canvas
+            UI.enabled = true;
+            pUI.enabled = false;
+            UIcam.enabled = true;
+            cam.enabled = false;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         //if we press the escape key
-        if (Input.GetKeyDown(KeyCode.Escape))
+        /*if (Input.GetKeyDown(KeyCode.Escape))
         {
             //pause the game
             Time.timeScale = 0;
             //show our pause menu canvas
             GetComponent<Canvas>().enabled = true;
-        }
+        }*/
     }
 
     public void ResumeGame()
     {
         //continue playing the game... somehow?
         Time.timeScale = 1;
-        GetComponent<Canvas>().enabled = false;
+        UI.enabled = false;
+        pUI.enabled = true;
+        UIcam.enabled = false;
+        cam.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
     public void QuitGame()
     {
